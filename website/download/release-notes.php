fa6b5657db5210d2456c526a21960a690e6198f2
henrih <henrih@09dcff7a-b715-0410-9601-b79a96267cd0>
2005-11-02 22:52:51 +0000

    Release 0.2 changes
    git-svn-id: https://svn.code.sf.net/p/oreka/svn/trunk@36 09dcff7a-b715-0410-9601-b79a96267cd0

--- website/download/release-notes.php
+++ website/download/release-notes.php
@@ -12,8 +12,19 @@
 ?>
 
 <h2><?=$pageTitle?></h2>
-
-<p>Oreka 0.1 is the first release of this software.</p>
+<p>2005-11-02 Oreka 0.2</p>
+<ul>
+<li>VoIp plugin now supports Cisco skinny (aka SCCP) protocol (including metadata extraction)</li>
+<li>VoIp plugin now supports Raw RTP sessions recording.</li>
+<li>VoIP plugin now tries to associate a SIP call to a capture port that is made of the actual local SIP agent (phone) IP address and TCP port. Before that, a capture port could be made of a gateway or external IP address and TCP port.</li>
+<li>Default to the last OS returned network device when config file does not specify or specifies invalid network device.</li>
+<li>Visual studio project now opens without errors.</li>
+<li>Fixed win32 crash when network device identifier in config file was invalid.</li>
+</ul>
+<p>2005-10-20 Oreka 0.1</p>
+<ul>
+<li>This is the first release of this software.</li>
+</ul>
 <?php
   include "../include/footer.inc.php";
 ?>
