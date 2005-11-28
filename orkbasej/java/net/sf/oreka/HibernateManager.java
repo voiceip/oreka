5d32d2b5d18b3d2048ea3e77d85a6bd023ba8337
henrih <henrih@09dcff7a-b715-0410-9601-b79a96267cd0>
2005-11-28 22:37:29 +0000

    Now creates user admin if does not exist already
    git-svn-id: https://svn.code.sf.net/p/oreka/svn/trunk@76 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkbasej/java/net/sf/oreka/HibernateManager.java
+++ orkbasej/java/net/sf/oreka/HibernateManager.java
@@ -29,14 +29,19 @@ import net.sf.oreka.persistent.RecTape;
 import net.sf.oreka.persistent.Service;
 import net.sf.oreka.persistent.User;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
 
 
 public class HibernateManager {
 	
 	private static SessionFactory sessionFactory = null;
+	static Logger logger = Logger.getLogger(HibernateManager.class);
 	
 	public static void configure(String filename) throws Exception {
 		
@@ -56,6 +61,38 @@ public class HibernateManager {
 		config.addAnnotatedClass(RecPort.class);
 		config.addAnnotatedClass(RecPortFace.class);
 		sessionFactory = config.buildSessionFactory();
+		
+		// Add admin user if does not exist yet
+		Session hbnSession = null;
+		Transaction tx = null;
+		try
+		{
+			hbnSession = getSession();
+			tx = hbnSession.beginTransaction();
+			User admin = (User)hbnSession.get(User.class, 1);
+			if(admin == null) {
+				admin = new User();
+				admin.setPassword("admin");
+				LoginString ls = new LoginString();
+				ls.setUser(admin);
+				ls.setLoginString("admin");
+				hbnSession.save(admin);
+				hbnSession.save(ls);				
+			}
+			tx.commit();
+		}
+		catch ( HibernateException he ) {
+			logger.log(Level.ERROR, he.toString());
+			he.printStackTrace();
+		}
+		catch (Exception e)
+		{
+			logger.error(e);
+			e.printStackTrace();
+		}
+		finally {
+			hbnSession.close();
+		}
 	}
 	
 	public static Session getSession() throws Exception {
