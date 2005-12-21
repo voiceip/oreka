1e54f09babd6a4e15d0d6ff2f632fe9fc0d391d8
henrih <henrih@09dcff7a-b715-0410-9601-b79a96267cd0>
2005-12-21 04:00:09 +0000

    First checkin
    git-svn-id: https://svn.code.sf.net/p/oreka/svn/trunk@112 09dcff7a-b715-0410-9601-b79a96267cd0

---
+++ orkbasej/java/net/sf/oreka/test/FillDatabaseProgram.java
@@ -0,0 +1,47 @@
+package net.sf.oreka.test;
+
+import java.util.List;
+
+import net.sf.oreka.HibernateManager;
+import net.sf.oreka.persistent.RecProgram;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class FillDatabaseProgram {
+
+	static HibernateManager hibernateManager = HibernateManager.instance();	
+	
+	public static void main(String args[]) throws Exception
+	{
+		hibernateManager.configure("c:/oreka/test/mysql.hbm.xml");
+		
+		Session hbnSession = hibernateManager.getSession();
+		Transaction tx = hbnSession.beginTransaction();
+		
+		RecProgram prg = new RecProgram();
+		prg.setDescription("my program 1");
+		prg.setName("myprog1");
+		hbnSession.save(prg);
+		
+		prg = new RecProgram();
+		prg.setDescription("my program 2");
+		prg.setName("myprog2");
+		hbnSession.save(prg);
+		
+		tx.commit();
+		
+		List recPrograms;
+		Criteria crit = hbnSession.createCriteria(RecProgram.class);
+		//crit.add( Expression.eq( "color", eg.Color.BLACK ) );
+		//crit.setMaxResults(10);
+		recPrograms = crit.list();
+		
+		
+		hbnSession.close();
+		
+		System.out.println("Done");
+	}
+	
+}
