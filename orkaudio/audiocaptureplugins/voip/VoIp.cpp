7ee5310c8eb7bcbdfa58306ae593de04e88394d8
Bruno Haas <bhaas@orecx.com>
2006-01-26 14:31:50 +0000

    Widended criteria for accepting RTP packet. TCP Port now need to be > 4000 and RTP payload type needs to be < 34
    git-svn-id: svn://svn.code.sf.net/p/oreka/svn/trunk@135 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkaudio/audiocaptureplugins/voip/VoIp.cpp
+++ orkaudio/audiocaptureplugins/voip/VoIp.cpp
@@ -147,7 +147,7 @@ bool TryRtp(EthernetHeaderStruct* ethernetHeader, IpHeaderStruct* ipHeader, UdpH
 		u_short dest = ntohs(udpHeader->dest);
 		if(!(ntohs(udpHeader->source)%2) && !(ntohs(udpHeader->dest)%2))	// udp ports must be even 
 		{
-			if(rtpHeader->pt == RTP_PT_PCMU || rtpHeader->pt == RTP_PT_PCMA)
+			if(rtpHeader->pt <= 34 )		// pt=34 is H263 and is the last possible valid codec 
 			{
 				result = true;
 				u_char* payload = (u_char *)rtpHeader + sizeof(RtpHeaderStruct);
@@ -352,7 +352,7 @@ void HandlePacket(u_char *param, const struct pcap_pkthdr *header, const u_char 
 	{
 		UdpHeaderStruct* udpHeader = (UdpHeaderStruct*)((char *)ipHeader + ipHeaderLength);
 
-		if(	ntohs(udpHeader->source) > 5000 && ntohs(udpHeader->dest) > 5000 )
+		if(	ntohs(udpHeader->source) > 4000 && ntohs(udpHeader->dest) > 4000 )
 		{
 			u_char* udpPayload = (u_char *)udpHeader + sizeof(UdpHeaderStruct);
 
