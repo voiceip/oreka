34ad771ae9c4585dc8be6ab280bca327f7010693
Bruno Haas <bhaas@orecx.com>
2006-03-28 22:20:18 +0000

    Removed timeout on semaphore acquire on linux. This was causing permanent 100% CPU.
    git-svn-id: svn://svn.code.sf.net/p/oreka/svn/trunk@204 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkaudio/ThreadSafeQueue.h
+++ orkaudio/ThreadSafeQueue.h
@@ -64,8 +64,13 @@ template <class T> bool ThreadSafeQueue<T>::push(T &element)
 /** Pop and element from the queue, or blocks until one available */
 template <class T> T ThreadSafeQueue<T>::pop()
 {
+// #### Fixme: when timeout specified under Linux CentOS 4.2, acquire returns immediately instead of waiting for the timeout -> causes CPU to spike at 100% 
+#ifdef WIN32
 	ACE_Time_Value timeout(time(NULL)+2);
 	m_semaphore.acquire(&timeout);
+#else
+        m_semaphore.acquire();
+#endif
 	MutexSentinel mutexSentinel(m_mutex);
 
 	T element;
