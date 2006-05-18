2956429fb5d3fa2e8acc802a1c8286bf767bab61
henrih <henrih@09dcff7a-b715-0410-9601-b79a96267cd0>
2006-05-18 01:57:20 +0000

    Non WIN32 platform does not try to sleep for a nanosecond between packets anymore in replay mode.
    git-svn-id: https://svn.code.sf.net/p/oreka/svn/trunk@234 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkaudio/audiocaptureplugins/voip/VoIp.cpp
+++ orkaudio/audiocaptureplugins/voip/VoIp.cpp
@@ -439,6 +439,7 @@ void HandlePacket(u_char *param, const struct pcap_pkthdr *header, const u_char 
 	int ipHeaderLength = ipHeader->ip_hl*4;
 	u_char* ipPacketEnd = (u_char*)ipHeader + ipHeader->ip_len;
 
+#ifdef WIN32
 	if(!s_liveCapture)
 	{
 		// This is a pcap file replay, make sure Orkaudio won't be flooded by too many
@@ -453,6 +454,7 @@ void HandlePacket(u_char *param, const struct pcap_pkthdr *header, const u_char 
         ts.tv_nsec = 1;
 		ACE_OS::nanosleep (&ts, NULL);
 	}
+#endif
 
 	if(DLLCONFIG.IsPacketWanted(ipHeader) == false)
 	{
