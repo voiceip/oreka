ac084c8ffb02965a8794d538c80581ae59b0c3c9
henrih <henrih@09dcff7a-b715-0410-9601-b79a96267cd0>
2006-06-21 20:48:22 +0000

    Moving LogManager.h to orkbasecxx
    git-svn-id: https://svn.code.sf.net/p/oreka/svn/trunk@278 09dcff7a-b715-0410-9601-b79a96267cd0

---
+++ orkbasecxx/LogManager.h
@@ -0,0 +1,46 @@
+/*
+ * Oreka -- A media capture and retrieval platform
+ * 
+ * Copyright (C) 2005, orecx LLC
+ *
+ * http://www.orecx.com
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License.
+ * Please refer to http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#ifndef __LOGMANAGER_H__
+#define __LOGMANAGER_H__
+
+#include <log4cxx/logger.h>
+#include "ace/Singleton.h"
+
+using namespace log4cxx;
+
+
+class OrkLogManager
+{
+public:
+	void Initialize();
+	void Shutdown();
+
+	LoggerPtr rootLog;
+	LoggerPtr topLog;
+	LoggerPtr immediateProcessingLog;
+	LoggerPtr batchProcessingLog;
+	LoggerPtr portLog;
+	LoggerPtr fileLog;
+	LoggerPtr reportingLog;
+	LoggerPtr configLog;
+	LoggerPtr tapelistLog;
+	LoggerPtr tapeLog;
+};
+
+typedef ACE_Singleton<OrkLogManager, ACE_Thread_Mutex> OrkLogManagerSingleton;
+
+#define LOG (*OrkLogManagerSingleton::instance())
+
+#endif
+
