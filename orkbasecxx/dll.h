629bdce3f9a850751ca96a9a63a11580d680af63
Bruno Haas <bhaas@orecx.com>
2006-06-21 19:13:33 +0000

    Moved __cdecl defines to new file dll.h
    git-svn-id: svn://svn.code.sf.net/p/oreka/svn/trunk@270 09dcff7a-b715-0410-9601-b79a96267cd0

---
+++ orkbasecxx/dll.h
@@ -0,0 +1,15 @@
+#ifndef __DLL_H__
+#define __DLL_H__
+
+#ifdef WIN32
+#define __CDECL__ __cdecl
+#else
+#define __CDECL__
+#endif
+#ifdef WIN32
+	#define DLL_EXPORT  __declspec( dllexport )
+#else
+	#define DLL_EXPORT
+#endif
+
+#endif
