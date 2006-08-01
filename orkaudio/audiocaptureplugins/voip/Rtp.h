155f85510ced7bad40a7562094d845b90e0ac319
henrih <henrih@09dcff7a-b715-0410-9601-b79a96267cd0>
2006-08-01 14:40:48 +0000

    Applying changeset 266 from 0.5 to trunk: Removed the unused old RTP mixer from the VoIp.dll
    git-svn-id: https://svn.code.sf.net/p/oreka/svn/trunk@319 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkaudio/audiocaptureplugins/voip/Rtp.h
+++ orkaudio/audiocaptureplugins/voip/Rtp.h
@@ -14,14 +14,8 @@
 #ifndef __RTP_H__
 #define __RTP_H__
 
-#include "AudioCapture.h"
 #include "boost/shared_ptr.hpp"
 #include "StdString.h"
-#include "LogManager.h"
-
-#define NUM_SAMPLES_CIRCULAR_BUFFER 8000
-#define NUM_SAMPLES_TRIGGER 4000			// when we have this number of available samples make a shipment
-#define NUM_SAMPLES_SHIPMENT_HOLDOFF 2000	// when shipping, ship everything but this number of samples 
 
 
 // useful info we extract from an RTP packet
@@ -45,36 +39,6 @@ public:
 };
 typedef boost::shared_ptr<RtpPacketInfo> RtpPacketInfoRef;
 
-
-// Ring buffer based on RTP timestamps
-// Gathers both sides of the conversation and mixes them into single channel
-// Robust to silence suppression
-// Supports RTP buffers of arbitrary sizes in both directions.
-class RtpRingBuffer
-{
-public:
-	RtpRingBuffer();
-	void AddRtpPacket(RtpPacketInfoRef&);
-	void SetCapturePort(CStdString& port);
-private:
-	void StoreRtpPacket(RtpPacketInfoRef&);
-	void CreateShipment(size_t silenceSize);
-	unsigned int FreeSpace();
-	unsigned int UsedSpace();
-	short* GetHoldOffPtr();
-	short* CircularPointerAddOffset(short *ptr, size_t offset);
-	short* CicularPointerSubtractOffset(short *ptr, size_t offset);
-
-	short* m_writePtr;		// pointer after newest RTP data we've received
-	short* m_readPtr;		// where to read from next
-	unsigned int m_readTimestamp;	// timestamp that the next shipment will have
-	unsigned int m_writeTimestamp;	// timestamp that the next RTP buffer should have
-	short* m_bufferEnd;
-	short m_buffer[NUM_SAMPLES_CIRCULAR_BUFFER];
-	CStdString m_capturePort;
-	LoggerPtr m_log;
-	unsigned int m_shippedSamples;
-};
 
 #endif
 
