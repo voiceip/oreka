ae76abfb51b3356115da42090b5364ad6a71a3a3
Bruno Haas <bhaas@orecx.com>
2008-08-05 18:04:57 +0000

    SIP is now also detected on port 8060 (as seen on Interactive Intelligence platform).
    git-svn-id: svn://svn.code.sf.net/p/oreka/svn/trunk@555 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkaudio/audiocaptureplugins/voip/VoIp.cpp
+++ orkaudio/audiocaptureplugins/voip/VoIp.cpp
@@ -1305,8 +1305,12 @@ bool TrySipTcp(EthernetHeaderStruct* ethernetHeader, IpHeaderStruct* ipHeader, T
 	std::list<SipTcpStreamRef> toErase;
 	u_char* startTcpPayload;
 
-	if(ntohs(tcpHeader->source) != 5060 && ntohs(tcpHeader->dest) != 5060)
+	if(ntohs(tcpHeader->source) != 5060 && ntohs(tcpHeader->dest) != 5060 &&
+		ntohs(tcpHeader->source) != 8060 && ntohs(tcpHeader->dest) != 8060    )	 
+	{
+		// interactive intelligence sometimes sends SIP traffic on port 8060
 		return false;
+	}
 
 	startTcpPayload = (u_char*)tcpHeader + (tcpHeader->off * 4);
 	tcpLengthPayloadLength = ((u_char*)ipHeader+ntohs(ipHeader->ip_len)) - startTcpPayload;
