f78524e3cc9b16b929e025e55da54121ec690428
Ralph Atallah <ratallah@orecx.com>
2008-11-26 21:02:28 +0000

    Combined hibernate and logging configuration files for orkweb and orktrack. Please refer to new documentation regarding manual deployment of orkweb and orktrack.
    git-svn-id: svn://svn.code.sf.net/p/oreka/svn/trunk@571 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkweb/src/net/sf/oreka/orkweb/ContextListener.java
+++ orkweb/src/net/sf/oreka/orkweb/ContextListener.java
@@ -17,8 +17,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import net.sf.oreka.HibernateManager;
-
 import org.apache.log4j.Logger;
 
 public class ContextListener implements ServletContextListener {
@@ -29,22 +27,33 @@ public class ContextListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 		
 		ServletContext context = servletContextEvent.getServletContext();
-		String log4jConfigFile = context.getInitParameter("Log4jConfigFile");
-		
-		if (log4jConfigFile == null) {
-			System.out.println("Log4jConfigFile context-param missing in web.xml");
+
+		String configFolder = context.getInitParameter("ConfigDirectory");
+		if (configFolder == null) {
+			configFolder = "c:/oreka/";
 		}
-		
-		LogManager.getInstance().configure(log4jConfigFile);
-		log = LogManager.getInstance().getRootLogger();
+
+		String log4jConfigFile = context.getInitParameter("Log4jConfigFile");	
+		if (log4jConfigFile == null) {
+			log.error("Log4jConfigFile context-param missing in web.xml");
+		} else {
+			log4jConfigFile = configFolder + "/" + log4jConfigFile;
+			LogManager.getInstance().configure(log4jConfigFile);
+			log = LogManager.getInstance().getRootLogger();
+			log.info("log4jConfigFile is " + log4jConfigFile);
+		}
+
 		log.info("========================================");
 		log.info("orkweb starting ...");
-			
+
 		String hibernateConfigFile = context.getInitParameter("HibernateConfigFile");
 		if (hibernateConfigFile == null) {
 			log.error("HibernateConfigFile context-param missing in web.xml");
-		}		
-		
+		} else {
+			log.info("HibernateConfigFile is " + hibernateConfigFile);
+			hibernateConfigFile  = configFolder + "/" + hibernateConfigFile ; 
+		}
+				
 		try {
 			OrkWeb.hibernateManager.configure(hibernateConfigFile);
 		}
