b1b56642353c0ebeb0ffd371676f54f7b5b8d347
beg_g <beg_g@09dcff7a-b715-0410-9601-b79a96267cd0>
2009-01-10 14:15:54 +0000

    Added support for the G722 codec. Tested in Linux so far.
    git-svn-id: https://svn.code.sf.net/p/oreka/svn/trunk@591 09dcff7a-b715-0410-9601-b79a96267cd0

---
+++ orkbasecxx/filters/g722codec/G722Codec.h
@@ -0,0 +1,41 @@
+/*
+ * Oreka -- A media capture and retrieval platform
+ *
+ * Copyright (C) 2005, orecx LLC
+ *
+ * http://www.orecx.com
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License.
+ * Please refer to http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+#ifndef __G722DECODER_H__
+#define __G722DECODER_H__ 1
+
+#include "LogManager.h"
+#include "Filter.h"
+#include "G722.h"
+
+class DLL_IMPORT_EXPORT_ORKBASE G722Decoder : public Filter
+{
+public:
+	G722Decoder();
+	~G722Decoder();
+
+	FilterRef __CDECL__ Instanciate();
+	void __CDECL__ AudioChunkIn(AudioChunkRef& chunk);
+	void __CDECL__ AudioChunkOut(AudioChunkRef& chunk);
+	AudioEncodingEnum __CDECL__ GetInputAudioEncoding();
+	AudioEncodingEnum __CDECL__ GetOutputAudioEncoding();
+	CStdString __CDECL__ GetName();
+	int __CDECL__ GetInputRtpPayloadType();
+	void __CDECL__ CaptureEventIn(CaptureEventRef& event);
+	void __CDECL__ CaptureEventOut(CaptureEventRef& event);
+
+private:
+	AudioChunkRef m_outputAudioChunk;
+	g722_decode_state_t m_ctx;
+};
+
+#endif
