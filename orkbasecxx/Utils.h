7d74b5de6afa0cb76e94663eba6f9336146cc854
Gerald Begumisa <gbegumisa@orecx.com>
2010-05-13 17:22:13 +0000

    Added mechanism for opening sockets on arbitrary TCP addresses.
    git-svn-id: svn://svn.code.sf.net/p/oreka/svn/trunk@706 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkbasecxx/Utils.h
+++ orkbasecxx/Utils.h
@@ -69,6 +69,9 @@ inline double StringToDouble(CStdString& value)
 bool DLL_IMPORT_EXPORT_ORKBASE StringIsDigit(CStdString& string);
 bool DLL_IMPORT_EXPORT_ORKBASE StringIsPhoneNumber(CStdString& string);
 bool DLL_IMPORT_EXPORT_ORKBASE MatchesStringList(CStdString& string, std::list<CStdString>& stringList);
+CStdString DLL_IMPORT_EXPORT_ORKBASE GetHostFromAddressPair(CStdString& hostname);
+int DLL_IMPORT_EXPORT_ORKBASE GetPortFromAddressPair(CStdString& hostname);
+CStdString DLL_IMPORT_EXPORT_ORKBASE FormatDataSize(unsigned long int size);
 
 //========================================================
 // file related stuff
