fd5ca9f74f1edbd58a2158b6370e3ccd4138c20a
Minh Nguyen <mnguyen@orecx.com>
2014-03-26 15:47:51 +0000

    voip: fixed crash issue in windows when there is no interface specified in <Devices>
    git-svn-id: svn://svn.code.sf.net/p/oreka/svn/trunk@889 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkaudio/audiocaptureplugins/voip/VoIp.cpp
+++ orkaudio/audiocaptureplugins/voip/VoIp.cpp
@@ -4274,7 +4274,7 @@ void VoIp::OpenDevices()
 				{
 					defaultDevice =  device;
 				}
-				if((*DLLCONFIG.m_devices.begin()).CompareNoCase("all") == 0 || DLLCONFIG.IsDeviceWanted(device->name))
+				if((DLLCONFIG.m_devices.size() > 0 && (*DLLCONFIG.m_devices.begin()).CompareNoCase("all") == 0) || DLLCONFIG.IsDeviceWanted(device->name))
 				{
 					// Open device
 					m_pcapHandle = pcap_open_live(device->name, 1500, PROMISCUOUS, 500, errorBuf);
