613feae260679b1b7c2000356a3017b7cacbff44
karapapa <karapapa@09dcff7a-b715-0410-9601-b79a96267cd0>
2015-08-25 20:10:42 +0000

    orkaudio: removed disabling log4cxx when testing
    git-svn-id: https://svn.code.sf.net/p/oreka/svn/trunk@942 09dcff7a-b715-0410-9601-b79a96267cd0

--- orkaudio/audiocaptureplugins/voip/SipParsers.cpp
+++ orkaudio/audiocaptureplugins/voip/SipParsers.cpp
@@ -19,12 +19,6 @@ static LoggerPtr s_rtcpPacketLog = Logger::getLogger("packet.rtcp");
 static LoggerPtr s_sipparsersLog = Logger::getLogger("packet.sipparsers");
 extern std::list<SipTcpStreamRef> s_SipTcpStreams;
 
-#ifdef TESTING	
-	#define LOG4CXX_INFO(a,b) (0)
-	#define LOG4CXX_DEBUG(a,b) (0)
-	#define LOG4CXX_WARN(a,b) (0)
-#endif
-
 bool TrySipBye(EthernetHeaderStruct* ethernetHeader, IpHeaderStruct* ipHeader, UdpHeaderStruct* udpHeader, u_char* udpPayload, u_char* packetEnd)
 {
 	bool result = false;
