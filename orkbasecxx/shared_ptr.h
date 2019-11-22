051eaf67d79ef60faa652028fdf2547f7e2426c9
Minh Nguyen <mnguyen@orecx.com>
2019-11-21 20:06:06 -0500

    orkbase: add weak pointer to oreka wrapper

--- orkbasecxx/shared_ptr.h
+++ orkbasecxx/shared_ptr.h
@@ -5,16 +5,18 @@
 namespace oreka {
 	using std::shared_ptr;
 	using std::make_shared;
+	using std::weak_ptr;
 };
 
 #else
 
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
-
+#include <boost/weak_ptr.hpp>
 namespace oreka {
 	using boost::shared_ptr;
 	using boost::make_shared;
+	using boost::weak_ptr;
 };
 
 #endif
