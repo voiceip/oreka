ebd543a53adc6a63511264489292bb03163a358a
kinshuk.bairagi <me@kinshuk.in>
2020-08-16 20:01:37 +0530

    Update build.sh

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -22,13 +22,3 @@ chmod +x make-orkaudio-deb.sh
 ./make-orkaudio-deb.sh
 
 ls -ltrah *.deb
-
-
-source /etc/os-release
-mv /oreka-src/.megarc ~/.megarc
-hash=$(git --git-dir /oreka-src/.git rev-parse --short @)
-echo "$hash"
-cat /etc/os-release
-mv orkaudio.deb "orkaudio-$VERSION_CODENAME-$hash.deb"
-ls -ltrah *.deb
-megaput --path "/Root/voiceip-oreka/" "orkaudio-$VERSION_CODENAME-$hash.deb"
