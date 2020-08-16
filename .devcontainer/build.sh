10944a4746d51c5db4cfd6165125cd618fc4dac7
kinshuk.bairagi <me@kinshuk.in>
2020-08-16 13:23:10 +0530

    create orkaudio deb

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-set -ex
+set -e
 
 #make orkbase
-cd /oreka/orkbasecxx
+cd /oreka-src/orkbasecxx
 autoreconf -i
 ./configure CXX=g++
 make
 make install
 
 #orkaudio
-cd /oreka/orkaudio
+cd /oreka-src/orkaudio
 autoreconf -i
 ./configure CXX=g++
 make
 make install
+
+#package orkaudio deb
+git clone https://github.com/voiceip/oreka-build.git /oreka-build
+cd /oreka-build/distribution
+chmod +x make-orkaudio-deb.sh
+./make-orkaudio-deb.sh
+
+ls -ltrah *.deb
 
