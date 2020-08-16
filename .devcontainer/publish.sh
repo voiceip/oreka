d953e2faef3ceea0e0e25d429e89227c5ae2c376
kinshuk.bairagi <me@kinshuk.in>
2020-08-16 19:47:44 +0530

    Update publish.sh

--- .devcontainer/publish.sh
+++ .devcontainer/publish.sh
@@ -4,6 +4,7 @@ source /etc/os-release
 mv /oreka-src/.megarc ~/.megarc
 hash=$(git --git-dir /oreka-src/.git rev-parse --short @)
 echo "$hash"
+cat /etc/os-release
 mv orkaudio.deb "orkaudio-$VERSION_CODENAME-$hash.deb"
 ls -ltrah *.deb
 megaput --path "/Root/voiceip-oreka/" "orkaudio-$VERSION_CODENAME-$hash.deb"
