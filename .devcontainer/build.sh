928e276a1a5f1b7895f6e9e6a9781adbcb1acabf
kinshuk.bairagi <me@kinshuk.in>
2020-08-16 19:49:14 +0530

    i am crazy

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -22,3 +22,13 @@ chmod +x make-orkaudio-deb.sh
 ./make-orkaudio-deb.sh
 
 ls -ltrah *.deb
+
+
+source /etc/os-release
+mv /oreka-src/.megarc ~/.megarc
+hash=$(git --git-dir /oreka-src/.git rev-parse --short @)
+echo "$hash"
+cat /etc/os-release
+mv orkaudio.deb "orkaudio-$VERSION_CODENAME-$hash.deb"
+ls -ltrah *.deb
+megaput --path "/Root/voiceip-oreka/" "orkaudio-$VERSION_CODENAME-$hash.deb"
