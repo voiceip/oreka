fb9acb584938ca55622865396dff7bc115ebdbd5
kinshuk.bairagi <me@kinshuk.in>
2020-08-16 19:19:58 +0530

    travis build matrix

--- .devcontainer/publish.sh
+++ .devcontainer/publish.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+source /etc/os-release
 mv /oreka-src/.megarc ~/.megarc
 hash=$(git --git-dir /oreka-src/.git rev-parse --short @)
 echo "$hash"
-mv orkaudio.deb "orkaudio-$hash.deb"
+mv orkaudio.deb "orkaudio-$VERSION_CODENAME-$hash.deb"
 ls -ltrah *.deb
-megaput --path "/Root/voiceip-oreka/" "orkaudio-$hash.deb"
+megaput --path "/Root/voiceip-oreka/" "orkaudio-$VERSION_CODENAME-$hash.deb"
