3bb7eea8905e842eb297164e4cb0c811ec4ea5ce
Kinshuk Bairagi <hi@kinsh.uk>
2022-06-30 01:45:14 +0530

    Update ConfigManager.cpp

--- orkbasecxx/ConfigManager.cpp
+++ orkbasecxx/ConfigManager.cpp
@@ -150,7 +150,7 @@ void ConfigManager::Initialize()
 				LOG4CXX_ERROR(LOG.configLog, CStdString("Could not parse config file:") + CONFIG_FILE_NAME);
 				failed = true;
 			}
-			doc->release();
+			// doc->release(); //throws Segmentation fault
 		}
 		else
 		{
